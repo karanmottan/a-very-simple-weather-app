f0b14dd1b833bf9fd1a1d837a5bc9f9f37341eed
karanmottan <karanmottan59@gmail.com>
2020-10-04 13:10:08 +0530

    Add existing file

---
+++ scripts/forecast.js
@@ -0,0 +1,31 @@
+
+const key = 'GDH0s3OdXG3FcSeVZRO6x6r1qU4dHLWe';
+
+// gets weather info
+
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+
+// gets city info
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+
+    const data = await response.json();
+
+    return data[0];
+};
+
